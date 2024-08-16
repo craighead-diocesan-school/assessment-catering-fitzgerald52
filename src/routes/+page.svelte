27c8dd4db31e93656d23e740c07d484509ad1b97
fitzgerald52 <124209843+fitzgerald52@users.noreply.github.com>
2024-08-16 15:19:18 +1200

    expected input
    tested getfood() function for the 3 breakfast menu items with expected input of loading the page and got the expect out put of seeing the price description name and image of all three items on the page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,24 @@
 <script>
   import Header from '$lib/Header.svelte'
+  let food = getFood()
+  async function getFood() {
+    let shopData = await fetch('https://digitech.craighead.school.nz/api/restaurant')
+    return shopData.json()
+  }
 </script>
 
 <Header />
 
 <main>
   <h2>SvelteKit</h2>
-
+  {#await food}
+    <!-- gives users an errror message when the page is loading  -->
+    waiting...
+  {:then food}
+    {#each food.breakfast as food}
+      {food.item} {food.description} {food.price} <img src={food.img} alt="" />
+    {/each}
+  {/await}
   <p>Welcome to coding with SvelteKit, a modern JavaScript framework that makes it easy to code great apps.</p>
 </main>
 
