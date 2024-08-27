681ae5188e6541b68f030e6801ffffd3ff8bc86a
fitzgerald52 <124209843+fitzgerald52@users.noreply.github.com>
2024-08-27 15:11:13 +1200

    expected inputs
    i tested the website with the new addition of it being organized using cards to make it more robust. i tested it with the expected inputs of adding the pancakes to the menu and got the expected inputs of it displaying the appropriate information including the gst

---
+++ src/lib/card.svelte
@@ -0,0 +1,20 @@
+<script>
+  export let food
+  export let inMenu
+  const gst = 0.15
+</script>
+
+{food.item}
+{food.description}
+{#if inMenu == true}
+  $ {food.price.toFixed(2)}+{(gst * food.price).toFixed(2)}
+{:else}
+  $ {food.price.toFixed(2)}
+{/if}
+<img src={food.img} alt="" />
+
+<style>
+  img {
+    padding: 5px;
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,19 +1,24 @@
 <script>
   import Header from '$lib/Header.svelte'
+  import Card from '../lib/card.svelte'
+  import food from '../lib/card.svelte'
+  // defines variables
   let foods = getFoods()
   let menu = []
-  let gst = 0.15
-  let menuName = ''
 
+  let menuName = ''
+  // gets information from provided website and stores it
   async function getFoods() {
     let shopData = await fetch('https://digitech.craighead.school.nz/api/restaurant')
     return shopData.json()
   }
+  // adds item to menu and switches food.selected to true
   function addToMenu(food) {
     menu = [...menu, food]
     food.selected = true
     foods = foods
   }
+  // removes item to menu and switches food.selected to false
   function removeFood(index, food) {
     menu = [...menu.slice(0, index), ...menu.slice(index + 1)]
     food.selected = false
@@ -22,6 +27,7 @@
 </script>
 
 <Header />
+<!-- //alows users to name their custom menu -->
 <p>input name of custom menu <input bind:value={menuName} /></p>
 
 <main>
@@ -31,69 +37,68 @@
         <!-- gives users an errror message when the page is loading  -->
         waiting...
       {:then foods}
+        <!-- //goes through each of the foods and displays the information required -->
         {#each foods.breakfast as food}
-          {food.item}
-          {food.description}
-          {#if !menu.includes(food)}
-            <button
-              on:click={() => {
-                addToMenu(food)
-              }}
-            >
-              add item to menu</button
-            >
-          {:else}
-            <p>this item is already in your menu</p>
-          {/if}
-
-          {food.price}
-          <div class:highlighted={food.selected}><img src={food.img} alt="" /></div>
+          <div class:highlighted={food.selected}>
+            <Card {food} />
+            {#if !menu.includes(food)}
+              <!-- //trigers add to menu function -->
+              <button
+                on:click={() => {
+                  addToMenu(food)
+                }}
+              >
+                add item to menu</button
+              >
+            {:else}
+              <!-- //gives error message if it is in the menu -->
+              <p>this item is already in your menu</p>
+            {/if}
+          </div>
         {/each}
         {#each foods.dinner as food}
-          {food.selected}
-          {food.item}
-          {food.description}
-          {food.price}
-          <div class:highlighted={food.selected}><img src={food.img} alt="" /></div>
-          {#if !menu.includes(food)}
-            <button
-              on:click={() => {
-                addToMenu(food)
-              }}
-            >
-              add item to menu</button
-            >
-          {:else}
-            <p>this item is already in your menu</p>
-          {/if}
-        {/each}{#each foods.dessert as food}
-          {food.item}
-          {food.description}
-          {food.price}
-          <div class:highlighted={food.selected}><img src={food.img} alt="" /></div>
-          {#if !menu.includes(food)}
-            <button
-              on:click={() => {
-                addToMenu(food)
-              }}
-            >
-              add item to menu</button
-            >
-          {:else}
-            <p>this item is already in your menu</p>
-          {/if}
+          <!-- //the class is highlighted only if food.selected is true -->
+          <div class:highlighted={food.selected}>
+            <Card {food} />
+
+            {#if !menu.includes(food)}
+              <button
+                on:click={() => {
+                  addToMenu(food)
+                }}
+              >
+                add item to menu</button
+              >
+            {:else}
+              <p>this item is already in your menu</p>
+            {/if}
+          </div>
+        {/each}
+        {#each foods.dessert as food}
+          <div class:highlighted={food.selected}>
+            <Card {food} />
+            {#if !menu.includes(food)}
+              <button
+                on:click={() => {
+                  addToMenu(food)
+                }}
+              >
+                add item to menu</button
+              >
+            {:else}
+              <p>this item is already in your menu</p>
+            {/if}
+          </div>
         {/each}
       {/await}
     </div>
     <div class="column">
+      <!-- //displays the users input from before -->
       <h1>Custom Menu: {menuName}</h1>
 
       {#each menu as food, index}
-        {food.item}
-        {food.description}
-        {food.price} <img src={food.img} alt="" />
-        {(gst * food.price).toFixed(2)}
-
+        <Card {food} inMenu={true} />
+        <!-- //trigers the remove food function -->
         <button
           on:click={() => {
             removeFood(index, food)
@@ -102,7 +107,7 @@
           delete food</button
         >
       {/each}
-
+      <!-- //gives error message if nothing is in the menu -->
       {#if menu.length == 0}
         <p>menu is empty, start adding food items</p>
       {/if}
@@ -112,6 +117,6 @@
 
 <style>
   .highlighted {
-    background-color: aqua;
+    background-color: rgb(255, 163, 237);
   }
 </style>
